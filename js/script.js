307067063c08fb6ea289ff98159976d3f3d0edc6
Bruno Murça <unibr@hotmail.com>
2024-10-29 23:07:29 -0300

    galeria

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function trocarTitulo() {
+
+const herois = document.querySelector('.galeria');
+console.log(herois.children[0].children[0]);
+
+const subTitulo = document.querySelector('h2');
+
+subTitulo.innerHTML += "<br>Herois";
+
+ }
+
